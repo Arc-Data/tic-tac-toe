7e4a802f87d9756a0cd8e2f400c77a801a9c116e
Arc-Data <arclight04@gmail.com>
2022-12-17 16:08:48 +0800

    redo javascript skeleton

--- script.js
+++ script.js
@@ -1,116 +1,31 @@
-const Player = (name, mark) => {
-	return {
-		name,
-		mark,
-	};
-};
-
 const gameBoard = (() => {
 	const board = Array.from(9).fill('');
-	const p1moves = [];
-	const p2moves = [];
 
-	let hasWinner = false;
-
-	const checkWin = () => hasWinner;
-
-	const getBoard = () => board;
-
-	const setCell = (idx, value) => {
-		if (board[idx]) return;
-		board[idx] = value;
-		displayController.render();
-		game.nextPlayer();
-	};
-
-	const resetBoard = () => {
-		console.log(board);
-		board.forEach((i, idx) => board[idx] = '');
-		console.log(board);
-
-		displayController.render();
-	};
-
-	return {
-		setCell,
-		resetBoard,
-		getBoard,
-		checkWin,
-	};
-
-})();
-
-const game = (() => {
-	const player1 = Player('Player 1', 'X');
-	const player2 = Player('Player 2', 'O');
-	let turnPlayer = player1;
-	let turn = 0;
-
-	const checkWin = () => {
-
-	};
-
-	const getTurnPlayer = () => turnPlayer;
-
-	const nextPlayer = () => {
-		turnPlayer = turnPlayer === player1 ? player2 : player1;
-		turn++;
-	};
-
-	return {
-		getTurnPlayer,
-		nextPlayer,
-	};
 })();
 
 const displayController = (() => {
-	const divs = document.querySelectorAll('.board > div');
-	const resetButton = document.querySelector('.btn');
-
-	resetButton.addEventListener('click', gameBoard.resetBoard);
+	const cells = document.querySelectorAll('.board > div');
 
 	const initialize = () => {
-		divs.forEach((div, idx) => {
-			div.dataset.index = idx;
-			div.addEventListener('click', () => gameBoard.setCell(div.dataset.index, game.getTurnPlayer().mark));
-		}); 	
-	};
-
-
-	const render = () => {
-		const board = gameBoard.getBoard();
-		divs.forEach((div, idx) => div.textContent = board[idx]);
+		cells.forEach(cell => {
+			cell.addEventListener('click', ());
+		});
 	};
 
 	initialize();
 
-	return {
-		render,
-	};
+
 })();
 
+const player = (() => {
+	const Person = (name, mark) => {
+		return {
+			name,
+			mark,
+		};
+	};
 
-/*
- * 	What do i need to consider?
- *	
- *  Player Turn - switch between player 1 and player 2
- *	Symbol 		- switch between x and o 
- *	if player 1 then symbol = x ? should that just be swept along every gamestate check?
- * 	div onclick change symbol and player ? 
- *  
- *  check all board divs and gather those with x's and o's
- *
- *
- *
- *
- *    
- *
- *
- *
- *
- *
- *
- *
- * 
- * a button that uses a function to reset the board 
- */
+	const player1 = Person('Player 1', 'X');
+	const player2 = Person('Player 2', 'O');
+
+})();
