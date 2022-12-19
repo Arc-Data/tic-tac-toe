9b753fcbd3014834db42ff9c2ea61e4d12cdcbca
Arc-Data <arclight04@gmail.com>
2022-12-19 18:03:15 +0800

    design reinvent

--- script.js
+++ script.js
@@ -18,55 +18,25 @@ const gameBoard = (() => {
 	const checkGameStatus = () => hasWinner;
 
 	const checkWinner = (moves) => {
-		hasWinner = winConditions.some(condition => {
-			return moves.every(index => condition.includes(index));
-		});
 
-		if(hasWinner) {
-			console.log("Huh?");
-		}
+
+		return hasWinner;
 	};
 
-	// const resetBoard = () => {
-	// 	board.forEach((item, idx) => board[idx] = '');
-	// 	displayController.render();
+	const resetBoard = () => {
+		board.forEach((i, idx) => board[idx] = '');
+		player.resetGameState();
+		displayController.render();
+		hasWinner = false;
 
-	// 	hasWinner = false;
-	// };
+		displayController.turnSubtitle(player.getTurnPlayerName());
+	};
 
 	return {
 		getBoard,
 		resetBoard,
 		checkWinner,
 		checkGameStatus,
-	};
-
-})();
-
-const displayController = (() => {
-	const cells = document.querySelectorAll('.board > div');
-	const resetButton = document.querySelector('.btn');
-
-	resetButton.addEventListener('click', gameBoard.resetBoard);
-
-	const initialize = () => {
-		cells.forEach((cell, idx) => {
-			cell.dataset.index = idx;
-			cell.addEventListener('click', () => player.setValue(cell));
-		});
-	};
-
-	initialize();
-
-	const render = () => {
-		const board = gameBoard.getBoard();
-		cells.forEach((cell, idx) => {
-			cell.textContent = board[idx];
-		});
-	};
-
-	return {
-		render,
 	};
 
 })();
@@ -81,12 +51,22 @@ const player = (() => {
 	};
 	const player1 = Person('Player 1', 'X', []);
 	const player2 = Person('Player 2', 'O', []);
+	
 	let turn = 1;
 	let turnPlayer = player1;
 
-	const resetMoves = () => {
+	const resetGameState = () => {
+		turnPlayer = player1;
+		turn = 1;
 		player1.moves = [];
 		player2.moves = [];
+
+		displayController.turnSubtitle(getTurnPlayerName);
+	};
+
+	const showPlayerMoves = () => {
+		console.log(player1.moves, player2.moves);
+		console.log(`Turn ${turn}`);
 	};
 
 	const nextPlayer = () => {
@@ -95,25 +75,75 @@ const player = (() => {
 
 	const setValue = (cell) => {
 		if(cell.textContent !== '' || gameBoard.checkGameStatus()) return;
-		
 		gameBoard.getBoard()[Number(cell.dataset.index)] = turnPlayer.mark;
 		turnPlayer.moves.push(Number(cell.dataset.index));
 		displayController.render();
-		
+		console.log(turn, turnPlayer.moves);
+		turn++;
+
 		if(turn >= 5) {
-			gameBoard.checkWinner(turnPlayer.moves);
-		}
+			if (gameBoard.checkWinner(turnPlayer.moves)) {
+				displayController.winSubtitle(turnPlayer.name);
+				return;
+			}
+		}	
 
 		nextPlayer();
-		turn++;
+		displayController.turnSubtitle(turnPlayer.name);
 		
 	};
 
+	const getTurnPlayerName = () => turnPlayer.name;
 	
 
 
 	return {
+		getTurnPlayerName,
+		resetGameState,
 		setValue,
+		showPlayerMoves,
 	};
 
 })();
+
+const displayController = (() => {
+	const subtitle = document.querySelector('#subtitle');
+	const cells = document.querySelectorAll('.board > div');
+	const resetButton = document.querySelector('.btn');
+
+	resetButton.addEventListener('click', gameBoard.resetBoard);
+
+	const initialize = () => {
+		cells.forEach((cell, idx) => {
+			cell.dataset.index = idx;
+			cell.addEventListener('click', () => player.setValue(cell));
+		});
+
+		turnSubtitle(player.getTurnPlayerName());
+	};
+
+	const winSubtitle = name => {
+		subtitle.textContent = `${name} wins!`;
+	};
+
+	const turnSubtitle = name => {
+		subtitle.textContent = `${name}'s turn`;
+	};
+
+	const render = () => {
+		const board = gameBoard.getBoard();
+		cells.forEach((cell, idx) => {
+			cell.textContent = board[idx];
+		});
+	};
+
+	initialize();
+
+	return {
+		turnSubtitle,
+		winSubtitle,
+		render,
+	};
+
+})();
+
